386688d76c6bd97ed6d08e775420ad7436bc85fe
Ben Smith-Stubbs <smubbs@users.noreply.github.com>
2017-07-21 18:04:29 +1000

    Initial commit

---
+++ quoteDatabase.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+
+
+quoteDatabase = {"We are the knights that say 'Ni!'": "Monty Python and the Holy Grail",
+                "Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.":  "Dennis the Peasant, Monty Python and the Holy Grail",
+                 "I’m sorry, Dave. I’m afraid I can’t do that.":  "HAL, 2001: A Space Odyssey",
+                 "Spock. This child is about to wipe out every living thing on Earth. Now, what do you suggest we do….spank it?": "Dr. McCoy, Star Trek: The Motion Picture",
+                 "If I’m not back in five minutes, just wait longer.": "Ace Ventura, Ace ventura, Pet Detective",
+                 "Bill, strange things are afoot at the Circle K.": "Ted, Bill & Ted’s Excellent Adventure",
+                 "Some days, you just can’t get rid of a bomb!": "Adam West, Batman & Robin",
+                 "Fantastic!": "The Doctor (Christopher Eccleston), Doctor Who",
+                 "This is the way society functions. Aren’t you a part of society?": "Kramer, Seinfeld",
+                 "Ray, if someone asks you if you’re a god, you say YES!": "Winston, Ghostbusters",
+                 "Greetings, programs!": "Flynn, TRON",
+                 "I find your lack of faith disturbing.": "Darth Vader, Star Wars",
+                 "Hokey religions and ancient weapons are no substitute for a good blaster at your side, kid.": "Han Solo, Star Wars",
+                 "Try not. Do, or do not. There is no try.": "Yoda, The Empire Strikes Back",
+                 "Worst. Episode. Ever.": "Comic Book Guy, The Simpsons",
+                 "Nothing shocks me–I’m a scientist.": "Indiana Jones, Indiana Jones and the Temple of Doom",
+                 "If we knew what it was we were doing, it would not be called research, would it?": "Albert Einstein",
+                 "Wait a minute, Doc. Ah… Are you telling me you built a time machine… out of a DeLorean?": "Marty McFly, Back to the Future",
+                 "Don’t call me a mindless philosopher, you overweight blob of grease!": "C3PO, Star Wars",
+                 "I’d just as soon kiss a wookiee!": "Princess Leia, The Empire Strikes Back",
+                 "I know kung fu.": "Neo, The Matrix",
+
+
+                 }
